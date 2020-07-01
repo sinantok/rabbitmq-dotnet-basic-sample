032c91f8b203b9c094f7117a2b46663b2692779e
Sinan Tok <sinantok@outlook.com>
2020-07-01 15:53:49 +0300

    commit try

---
+++ RabbitMQSample/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQSample
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ RabbitMQSample/Publisher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQSample
+{
+    public class Publisher
+    {
+    }
+}
